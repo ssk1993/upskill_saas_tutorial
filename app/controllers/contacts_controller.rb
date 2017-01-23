c8de7642a3c833ea4f9f7ece76787c6059265fb9
sugandh khobragade <sugandhskhobragade@gmail.com>
2017-01-23 23:33:09 +0000

    Added Contact form mailer view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,6 +6,12 @@ end
 def create
   @contact = Contact.new(contact_params)
   if @contact.save
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params [:contact][:comments]
+      
+      
+      ContactMailer.contact_email(name,email,body).deliver
      flash[:success] = "Message sent."
   else
      flash[:danger] = @contact.errors.full_messages.join(", ")
